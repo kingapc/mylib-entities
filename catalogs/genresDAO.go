490cc67db69fe1cbbb23d2bd200b3837707023b0
Reinaldo Pineda <Reinaldo.Pineda@focusservices.com>
2021-10-17 20:19:36 -0600

    first commit

---
+++ catalogs/genresDAO.go
@@ -0,0 +1,42 @@
+package catalogs
+
+import (
+	conn "github.com/rpinedafocus/mylib-dbconn"
+)
+
+type Genres struct {
+	name string
+}
+
+func Create(newGenre Genres) bool {
+
+	db, errc := conn.GetConnection()
+
+	if errc {
+		return false
+	} else {
+		sqlStatement := `INSERT INTO university."genres" (name) VALUES ($1)`
+		_, err := db.Exec(sqlStatement, newGenre.name)
+
+		if err != nil {
+			return false
+		} else {
+			return true
+		}
+	}
+}
+
+/*
+func main() {
+	test := Genres{}
+	test.name = "Test Genre"
+
+	result := Create(test)
+
+	if result {
+		fmt.Printf("\nRegistro creado\n")
+	} else {
+		fmt.Printf("\nRegistro no creado\n")
+	}
+}
+*/
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rpinedafocus/mylib-entities
+
+go 1.17
+
+require (
+	github.com/lib/pq v1.10.3 // indirect
+	github.com/rpinedafocus/mylib-dbconn v0.0.0-20211018012155-d4e63cb3964e // indirect
+)
